8d9c0c65d8d5c39682d630c7b8a1d0059bb59e4e
Optimism Web Solutions <26356880+optimism2011@users.noreply.github.com>
2018-06-20 20:49:09 +0530

    Initial Commit
    Initial Commit

---
+++ MyFirstCoreProject/Models/ApplicationDBContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyFirstCoreProject.Models
+{   
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
